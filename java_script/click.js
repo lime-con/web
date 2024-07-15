5e0a1fcbb8c2a39b91156b6f064606330346558c
lime-con <147431327+lime-con@users.noreply.github.com>
2024-07-15 22:03:19 +0300

    Add files via upload

---
+++ java_script/click.js
@@ -0,0 +1,9 @@
+function usb(){
+    document.getElementById("win_2").innerHTML = "<iframe class='win_all' src='tools/usb.html' frameborder='1'></iframe>";
+}
+function ddos_of_login_fack(){
+    document.getElementById("win_2").innerHTML = "<iframe class='win_all' src='tools/ddos.html' frameborder='1'></iframe>";
+}
+function usb(){
+    document.getElementById("win_2").innerHTML = "<iframe class='win_all' src='tools/usb.html' frameborder='1'></iframe>";
+}
